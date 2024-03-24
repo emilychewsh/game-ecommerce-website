9f5402171292b9b0cae7655ace0498c45160f98b
Emily Chew <emilychewsh@gmail.com>
2024-03-24 18:13:10 +1100

    add hades and scroll css

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -5,14 +5,14 @@ import './gameCard.css'
 
 function GameCard( {game} ) {
   return (
-    <div className="col-xl-3 col-lg-4 col-md-6">
+    <div className="col-xl-3 col-lg-4 col-md-6 overflow-hidden">
         <div className='gameCard'>
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={game.img} />
                 <Card.Body>
                     <Card.Title className='gameTitle'>{game.title}</Card.Title>
                     <Card.Text className='gamePrice'>
-                        {game.price}
+                        {`A$ ${game.price}`}
                     </Card.Text>
                     <Button variant="primary">Add To Cart</Button>
                     <Button variant="primary" className='like'>
